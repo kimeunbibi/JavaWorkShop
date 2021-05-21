89d0880fb58de75d82349be8c7bc99703f0bdad0
bitcamp <bitcamp@DESKTOP-8O6S4JH>
2021-05-21 13:08:01 +0900

    ok

---
+++ MyJavaWorkShop2/src/com/test/EX149p.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class EX149p {
+
+	public static void main(String[] args) {
+		System.out.println("현재 월을 입력하세요.");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+
+}
---
+++ MyJavaWorkShop2/src/com/test/aaaa.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class aaaa {
+	public static void main(String[] args) {
+		char c = 'a';
+		for(int i=0; i<26; i++) {
+			System.out.print(c++);
+		}
+		System.out.println();
+		
+		c = 'A';
+		for(int i=0; i<26; i++) {
+			System.out.print(c++);
+		}
+		System.out.println();
+		
+		c='0';
+		for(int i=0; i<10; i++) {
+			System.out.print(c++);
+		}
+		
+		System.out.println();
+	}
+
+}

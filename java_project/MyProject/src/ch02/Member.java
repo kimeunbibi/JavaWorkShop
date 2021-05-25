d524aeca3912162a268ec47f6d82e51388ff4345
kimeunbibi <84178283+kimeunbibi@users.noreply.github.com>
2021-05-25 10:34:46 +0900

    n

--- java_project/MyProject/src/ch02/Member.java
+++ java_project/MyProject/src/ch02/Member.java
@@ -8,6 +8,11 @@ public class Member {
 		// 데이터의 타입 식별 이름
 		// 데이터 타입 -> 내가 다루어야 하는 데이터의 특징에 의해서 결정
 		// 기본형 타입 8가지 -> 정수형, 실수형, bool, 문자
+		String name = "손흥민";
+		int age = 27;
+		double height = 185.3d;
+		
+
 	}
 
 }

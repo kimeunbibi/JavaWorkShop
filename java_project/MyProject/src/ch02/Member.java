ba310f11e7301fcd7f17173d5b42d86bfc3e27b9
bitcamp <bitcamp@DESKTOP-8O6S4JH>
2021-05-25 10:02:03 +0900

    m

---
+++ java_project/MyProject/src/ch02/Member.java
@@ -0,0 +1,13 @@
+package ch02;
+
+// ch02.Member
+public class Member {
+
+	public static void main(String[] args) {
+		// 변수 선언
+		// 데이터의 타입 식별 이름
+		// 데이터 타입 -> 내가 다루어야 하는 데이터의 특징에 의해서 결정
+		// 기본형 타입 8가지 -> 정수형, 실수형, bool, 문자
+	}
+
+}
